08b9453ac0e54b0c1cdd2e88526c0470b90fa8e4
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-04-24 18:00:11 +0200

    correct application.rb to be in line with default for rails

--- config/application.rb
+++ config/application.rb
@@ -20,19 +20,23 @@ Bundler.require(*Rails.groups)
 
 module PompaApi
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
+
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Custom configuration
-    config.pompa = Hashie::Mash.new(Rails.application.config_for(:pompa_defaults))
-    config.pompa.merge!(Hashie::Mash.new(Rails.application.config_for(:pompa)))
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Custom configuration
+    config.pompa = Hashie::Mash.new(Rails.application.config_for(:pompa_defaults))
+    config.pompa.merge!(Hashie::Mash.new(Rails.application.config_for(:pompa)))
 
     # Use SQL for ActiveRecord schema
     config.active_record.schema_format = :sql
