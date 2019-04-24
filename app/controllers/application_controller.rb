e22a6f794ee9a606dd61ab3299524695c016cde1
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-04-24 23:53:34 +0200

    add default value to error handlers' arguments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,31 +19,33 @@ class ApplicationController < ActionController::API
   REFERENCED = 'is referenced'.freeze
 
   protected
-    def record_not_found(error)
+    def record_not_found(error = nil)
       render :json => { :errors => { :record => [NOT_FOUND] } },
         :status => :not_found
     end
 
-    def record_not_unique(error)
+    def record_not_unique(error = nil)
       render :json => { :errors => { :record => [NOT_UNIQUE] } },
         :status => :unprocessable_entity
     end
 
-    def record_invalid(error)
+    def record_invalid(error = nil)
       render :json => { :errors => { :record => [INVALID] } },
         :status => :unprocessable_entity
     end
 
-    def record_referenced(error)
+    def record_referenced(error = nil)
       render :json => { :errors => { :record => [REFERENCED] } },
         :status => :unprocessable_entity
     end
 
-    def query_invalid(error)
+    def query_invalid(error = nil)
       render :json => { :errors => { :query => [INVALID] } },
         :status => :unprocessable_entity
 
-      multi_logger.error{"Error in #{self.class.name}, #{error.class}: #{error.message}"}
-      multi_logger.backtrace(error)
+      if !error.nil
+        multi_logger.error{"Error in #{self.class.name}, #{error.class}: #{error.message}"}
+        multi_logger.backtrace(error)
+      end
     end
 end
