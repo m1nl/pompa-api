995867f185295c9e606b3295b03fd8af787e9695
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-11-28 23:27:22 +0100

    add better role handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,40 @@
 class User < ApplicationRecord
+  before_validation :normalize_roles
+  validate :roles_check
 
   class Roles
-    AUTH='AUTH'.freeze
+    AUTH = 'AUTH'.freeze
+
+    def self.all
+      return @all_roles if !@all_roles.nil?
+
+      @all_roles = []
+      constants.each do |c|
+        @all_roles << const_get(c)
+      end
+
+      return @all_roles
+    end
   end
 
   def has_role?(role)
     roles.include?(role)
   end
+
+  private
+    def normalize_roles
+      roles ||= []
+    end
+
+    def roles_check
+      return if !roles_changed?
+      return if roles.blank?
+
+      roles.each do |r|
+        if !Roles.all.include?(r)
+          errors.add(:roles,
+            "invalid role #{r}")
+        end
+      end
+   end
 end
