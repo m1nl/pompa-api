b50a324aa0ea11fd57f590233e7b81cd7a6ebe94
Mateusz Nalewajski <mateusz@nalewajski.pl>
2022-01-24 02:07:40 +0100

    Revert "do not touch activerecord connection pool for sidekiq"
    This reverts commit 12c33bb902114c94b075c1c410a05f47f0792041.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,6 +10,19 @@ Sidekiq.configure_server do |config|
   Rails.application.config.cache_classes = true
   Rails.application.config.eager_load = true
 
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  pool_size = Sidekiq.options[:concurrency] + 5
+
+  ActiveSupport.on_load(:active_record) do
+    active_record_config = ActiveRecord::Base.configurations
+      .find_db_config(Rails.env).configuration_hash.deep_dup
+
+    active_record_config['pool'] = pool_size
+
+    ActiveRecord::Base.establish_connection(active_record_config.freeze)
+  end
+
   config.redis = Pompa::RedisConnection.pool(
     :db => Pompa::RedisConnection::SIDEKIQ_DB,
     :pool_size => pool_size,
